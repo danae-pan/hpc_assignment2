ac744ded88edea7db9081e0149d9104035787c6f
danae-pan <danai231@windowslive.com>
2025-01-17 17:55:48 +0100

    feat: convergence

--- run_jacobi_convergence.bash
+++ run_jacobi_convergence.bash
@@ -1,5 +1,15 @@
 #!/bin/bash
+#BSUB -J jacobi_opt_benchmark
+#BSUB -o jacobi_opt_benchmark_%j.out
+#BSUB -q hpcintro
+#BSUB -n 24
+#BSUB -R "rusage[mem=10GB]"
+#BSUB -W 00:30
+# assures that your job has a whole CPU for itself (shared L3 cache)
+#BSUB -R "span[hosts=1]"
 
+# Use ⁠ nodestat -F hpcintro ⁠ to figure out what models are available
+#BSUB -R "select[model == XeonE5_2650v4]"
 OUTPUT_FILE="jacobi_convergence.data"
 
 # Define grid sizes and tolerances
